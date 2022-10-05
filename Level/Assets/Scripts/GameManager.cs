701baec1cbdccec683ce16aa012f2cb4ee2f1bf3
AustinAndersonFullSail <98759320+AustinAndersonFullSail@users.noreply.github.com>
2022-10-05 11:27:44 -0600

    Game Manager
    Update function completed

--- Level/Assets/Scripts/GameManager.cs
+++ Level/Assets/Scripts/GameManager.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    [Header("----- Player -----")]
+    public GameObject player;
+    //public PlayerController playerScript;
+
+    [Header("----- UI -----")]
+    public GameObject pauseMenu;
+    public GameObject menuCurrentlyOpen;
+
+    public bool isPaused;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        instance = this;
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        //playerScript = player.GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Cancel"))
+        {
+            isPaused = !isPaused;
+            pauseMenu.SetActive(isPaused);
+
+            if (isPaused)
+                CursorLockPause();
+            else
+                CursorUnLockUnPause();
+        }
     }
 }
