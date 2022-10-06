fa41616a65f44feaa4066117db3b863c81da2a5d
Patrick Thayer <94177293+PatrickThayer@users.noreply.github.com>
2022-10-06 17:24:16 -0400

    Implemented Movement method.

--- Level/Assets/Scripts/playerController.cs
+++ Level/Assets/Scripts/playerController.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
 
+    [Header("----- Components -----")]
+    [SerializeField] CharacterController controller;
+
+
+    [Header("----- Player Stats -----")]
+    [Range(1, 5)] [SerializeField] float playerSpeed;
+    [Range(8, 15)] [SerializeField] float jumpHeight;
+    [Range(-5, -35)] [SerializeField] float gravityValue;
+    [Range(1, 3)] [SerializeField] int jumpsMax;
+
+    private Vector3 playerVelocity;
+    private int timesJumped;
+
+
     void Start()
     {
         
@@ -13,6 +27,31 @@ public class playerController : MonoBehaviour
 
     void Update()
     {
-        
+       movement();
+    }
+
+    void movement()
+    {
+        if (controller.isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+            timesJumped = 0;
+        }
+
+        Vector3 move = transform.right * Input.GetAxis("Horizontal") +
+                       transform.forward * Input.GetAxis("Vertical");
+
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && timesJumped < jumpsMax)
+        {
+            playerVelocity.y = jumpHeight;
+            timesJumped++;
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
