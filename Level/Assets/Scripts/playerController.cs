27c4b329c1c05e262845ccad945f7063c7ccee67
Patrick Thayer <94177293+PatrickThayer@users.noreply.github.com>
2022-10-06 16:49:56 -0400

    Set up stage and make script.

---
+++ Level/Assets/Scripts/playerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
