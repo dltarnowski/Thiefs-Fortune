276efc5f1e70cc846ce2e1d06f9155e0571a6278
Shem-aiah <SINelson@student.fullsail.edu>
2022-10-13 16:40:01 -0400

    Returns to Main menu

--- Level/Assets/Scripts/MenuButtons.cs
+++ Level/Assets/Scripts/MenuButtons.cs
@@ -36,7 +36,7 @@ public class MenuButtons : MonoBehaviour
     public void Return()
     {
         Time.timeScale = 1;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
     }
     public void respawn()
     {
