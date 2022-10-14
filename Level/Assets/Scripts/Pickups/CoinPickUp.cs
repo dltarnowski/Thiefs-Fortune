44970d6dac5dfd82e52208c4ae749a6be770ace5
Shem-aiah <SINelson@student.fullsail.edu>
2022-10-14 12:39:30 -0400

    Functioning coin pick ups

---
+++ Level/Assets/Scripts/Pickups/CoinPickUp.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickUp : MonoBehaviour
+{
+    int coinValue = 1;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameManager.instance.currencyNumber += coinValue;
+            Debug.Log(gameManager.instance.currencyNumber);
+            gameManager.instance.playerScript.updatePlayerHUD();
+            Destroy(gameObject);
+        }
+    }
+}
