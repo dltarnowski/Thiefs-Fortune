1c671d43785fc1bf38104de85db47aeaec712cd9
Nicholas Taveras <taveras123n@gmail.com>
2022-10-28 01:22:55 -0400

    Added Use Function

--- Level/Assets/Scripts/Inventory/Weapon.cs
+++ Level/Assets/Scripts/Inventory/Weapon.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 
-public class Weapon : Item
+public abstract class Weapon : Item
 {
     //Stats
+    public WeaponSlot weaponSlot;
     public float speed;
     public GameObject model;
     public AudioClip sound;
@@ -13,4 +14,13 @@ public class Weapon : Item
     public float recoilX, recoilY, recoilZ;
     public float snappiness;
     public float returnSpeed;
+
+    public override void Use()
+    {
+        base.Use();
+        EquipmentManager.instance.Equip(this);
+        RemoveFromInventory();
+    }
 }
+
+public enum WeaponSlot { Gun1, Gun2, Sword1, Sword2 }
