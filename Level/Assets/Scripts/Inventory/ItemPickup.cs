c833e4b456e0083ae566cb3d28dc918698d48b2f
Nicholas Taveras <taveras123n@gmail.com>
2022-10-28 01:23:44 -0400

    Created Universal Item Pickup Script

---
+++ Level/Assets/Scripts/Inventory/ItemPickup.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    [SerializeField] Item item;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Inventory.instance.Add(item);
+
+            Destroy(gameObject);
+        }
+    }
+}
