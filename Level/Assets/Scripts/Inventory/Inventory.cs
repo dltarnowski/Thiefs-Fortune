8b95e553849a10a89615756a306903357150ff0b
Nicholas Taveras <taveras123n@gmail.com>
2022-10-28 01:25:04 -0400

    Made it so there can only be one instance of inventory and added delegat to check if ItemChanged

--- Level/Assets/Scripts/Inventory/Inventory.cs
+++ Level/Assets/Scripts/Inventory/Inventory.cs
@@ -4,29 +4,49 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    public static Inventory instance;
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+    public int space = 6;
+
     public List<Item> items = new List<Item>();
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if(instance != null)
+        {
+            Debug.Log("More than one instance of Inventory Found");
+            return;
+        }
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool Add(Item item)
     {
-        
-    }
+        if(!item.isDefaultItem)
+        {
+            if(items.Count >= space)
+            {
+                Debug.Log("Not enough room");
+                return false;
+            }
 
-    public void Add(Item item)
-    {
-        Debug.Log("Item Added");
-        items.Add(item);
+            items.Add(item);
+
+            if(onItemChangedCallback != null)
+                onItemChangedCallback.Invoke();
+        }
+        return true;
     }
 
     public void Remove(Item item)
     {
         items.Remove(item);
+
+        if (onItemChangedCallback != null)
+            onItemChangedCallback.Invoke();
     }
 
 }
