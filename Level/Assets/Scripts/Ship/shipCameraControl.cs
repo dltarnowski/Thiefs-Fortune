503acc091736ffda484b41049131c6ae98a30429
Nicholas Taveras <taveras123n@gmail.com>
2022-10-30 12:42:47 -0400

    Camera script for ships

---
+++ Level/Assets/Scripts/Ship/shipCameraControl.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipCameraControl : MonoBehaviour
+{
+    [SerializeField] public int sensHort;
+    [SerializeField] public int sensVert;
+
+    int lockVertMin;
+    int lockVertMax;
+
+    [SerializeField] bool invert;
+
+    float xRotation;
+    int currSliderValue;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void LateUpdate()
+    {
+        // get input
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensHort;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensVert;
+
+        if (invert)
+            xRotation += mouseY;
+        else
+            xRotation -= mouseY;
+
+        // clamp camera rotation
+        xRotation = Mathf.Clamp(xRotation, 10, 10);
+
+        // rotate the camera on the x-axis
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        // rotate the player
+        transform.parent.Rotate(Vector3.up * mouseX);
+
+    }
+}
