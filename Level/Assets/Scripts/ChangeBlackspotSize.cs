e2aec39239b8e19ab42477e5a2dc41fd39c8ea52
Patrick Thayer <94177293+PatrickThayer@users.noreply.github.com>
2022-10-30 16:55:47 -0400

    Added blackspot to player prefab and implemented a scale change function for said blackspot.

---
+++ Level/Assets/Scripts/ChangeBlackspotSize.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBlackspotSize : MonoBehaviour
+{
+    float blackspotMultiplier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blackspotMultiplier = gameManager.instance.blackspot.blackSpotMultiplier;
+        gameObject.transform.localScale = new Vector3(5.0f * blackspotMultiplier, 0.01f, 5.0f * blackspotMultiplier);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (blackspotMultiplier != gameManager.instance.blackspot.blackSpotMultiplier)
+        {
+            blackspotMultiplier = gameManager.instance.blackspot.blackSpotMultiplier;
+            gameObject.transform.localScale = new Vector3(5.0f * blackspotMultiplier, 0.01f, 5.0f * blackspotMultiplier);
+        }
+    }
+}
