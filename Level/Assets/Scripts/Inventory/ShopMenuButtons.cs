8e3348c5d11d55e950cf381ce3912526a4a9128a
dltarnowski <dltarnowski@student.fullsail.edu>
2022-10-31 17:13:56 -0400

    Revert "Revert "Merge pull request #365 from dltarnowski/Level-Design""
    This reverts commit 82adde667180b7bb520057a92c77773b971a7a9c.

--- Level/Assets/Scripts/Inventory/ShopMenuButtons.cs
+++ Level/Assets/Scripts/Inventory/ShopMenuButtons.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class ShopMenuButtons : MonoBehaviour
 {
-    ShopInventory inventory;
+    public static ShopInventory inventory;
     public GameObject shopMenu;
     public GameObject playerInventory;
     public GameObject buyInventory;
@@ -20,8 +20,8 @@ public class ShopMenuButtons : MonoBehaviour
     }
     public void Talk()
     {
-        NPCManager.instance.dialogue.enabled = false;
-        NPCManager.instance.followUpDialogue.enabled = true;
+        NPCManager.instance.dialogue.gameObject.SetActive(false);
+        NPCManager.instance.followUpDialogue.gameObject.SetActive(true);
     }
 
     public void Shop()
@@ -56,7 +56,7 @@ public class ShopMenuButtons : MonoBehaviour
         sellInventory.SetActive(false);
         buyTab.interactable = false;
         sellTab.interactable = true;
-        inventory.onItemChangedCallback.Invoke();
+        Inventory.instance.onItemChangedCallback.Invoke();
     }
 
     public void SellTab()
@@ -65,6 +65,6 @@ public class ShopMenuButtons : MonoBehaviour
         sellInventory.SetActive(true);
         buyTab.interactable = true;
         sellTab.interactable = false;
-        inventory.onItemChangedCallback.Invoke();
+        Inventory.instance.onItemChangedCallback.Invoke();
     }
 }
