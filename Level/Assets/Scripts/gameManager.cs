2dd3840d522114cf033cc9c1c8734b2b11a55ecb
dltarnowski <dltarnowski@student.fullsail.edu>
2022-11-01 16:41:26 -0400

    NPC dialogue Prefabs created
    Instructions:
    - Add npcDialogueBox prefab into UI
    - Add NPC AI Prefab onto npc that you would like interactable
    - npcCam needs to be added to gameManager

--- Level/Assets/Scripts/gameManager.cs
+++ Level/Assets/Scripts/gameManager.cs
@@ -62,6 +62,8 @@ public class gameManager : MonoBehaviour
     public TextMeshProUGUI coinCountText;
     public bool weaponCollide;
     public bool consumeCollide;
+    public bool npcCollide;
+    public GameObject npcCam;
 
     [Header("----- Gun -----")]
     public GameObject mainCamera;
