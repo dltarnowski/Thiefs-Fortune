5a8b588afe2eaf9945ae20e7f05ca7744516e083
AustinAndersonFullSail <98759320+AustinAndersonFullSail@users.noreply.github.com>
2022-11-04 15:41:52 -0600

    Update BobAndSpin.cs
    bob and spin does not override transform anymore

--- Level/Assets/Scripts/Pickups/BobAndSpin.cs
+++ Level/Assets/Scripts/Pickups/BobAndSpin.cs
@@ -4,22 +4,18 @@ using UnityEngine;
 
 public class BobAndSpin : MonoBehaviour
 {
-    Vector3 originalPos;
+
+    /////////**************** Height needs to be about twice the speed for it to work correctly ******************/////////////
 
     [SerializeField] float bounceHeight = .5f;
     [SerializeField] float bounceSpeed = .25f;
     [SerializeField] int rotationSpeed = 45;
 
-    private void Start()
-    {
-        originalPos = gameObject.transform.position;
-    }
-
+    
     private void FixedUpdate()
     {
-        gameObject.transform.position = new Vector3(originalPos.x,
-                                                    Mathf.PingPong(bounceSpeed * Time.time, bounceHeight) + originalPos.y,
-                                                    originalPos.z);
+        gameObject.transform.Translate( new Vector3(0, (Mathf.PingPong(bounceSpeed * Time.time, bounceHeight) - bounceHeight/2) /100, 0));
+
         gameObject.transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
     }
 }
