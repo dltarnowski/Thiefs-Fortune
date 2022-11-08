7f2a46c1c2a5ba4df75e25c8aa538389baf27569
Shem-aiah <SINelson@student.fullsail.edu>
2022-11-07 20:41:00 -0500

    Added Note

--- Level/Assets/Scripts/gameManager.cs
+++ Level/Assets/Scripts/gameManager.cs
@@ -74,6 +74,7 @@ public class gameManager : MonoBehaviour
 
     [Header("----- Other -----")]
     public bool isPaused;
+    public GameObject importantNote;
     public bool crossHairVisible = true;
     public Slider MSSlider;
     public Slider MusicSlider;
