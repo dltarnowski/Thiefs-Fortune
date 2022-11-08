d7029d99807ce739d64843bc1a229cb5c8d5d3d0
dltarnowski <dltarnowski@student.fullsail.edu>
2022-11-07 21:41:32 -0500

    Fixed warnings/errors in Tutorial Island

--- Level/Assets/Scripts/enemy/meleeDamage.cs
+++ Level/Assets/Scripts/enemy/meleeDamage.cs
@@ -8,7 +8,7 @@ public class meleeDamage : MonoBehaviour
     [SerializeField] meleeEnemyAI EAI;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && gameObject != null)
             gameManager.instance.playerScript.takeDamage(EAI.swordStat.strength);
     }
 }
